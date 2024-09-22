088b91b8353efd4b8f1789e8f6c99f2233bc705f
Daniel Ju <DahunJu@my.unt.edu>
2024-09-22 11:38:58 -0500

    Improving code cleaness

--- net/daniel/plot/cmds/deleteExpiredCommand.java
+++ net/daniel/plot/cmds/deleteExpiredCommand.java
@@ -1,6 +1,7 @@
 package net.daniel.plot.cmds;
 
 import java.util.ArrayList;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,91 +15,88 @@ import net.daniel.plotcmd.Utils.MCUtils;
 
 public class deleteExpiredCommand implements CommandExecutor {
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-		if (sender.hasPermission("MinePlotCMD.resetExpired")) {
+        if (!sender.hasPermission("MinePlotCMD.resetExpired")) {
+            sender.sendMessage(Lang.NO_PERM.toString());
+            return true;
 
-			if (args.length == 0) {
+        }
 
-				(new BukkitRunnable() {
-					public void run() {
+        if (args.length == 0) {
 
-						long start = System.currentTimeMillis();
-						ArrayList<Plot> expiredPlots = new ArrayList<Plot>();
+            sender.sendMessage(Lang.DELETE_EXPIRED_HELP.toString());
+            return true;
 
-						ArrayList<Plot> deletedPlots = new ArrayList<Plot>();
-						ArrayList<Plot> failedPlots = new ArrayList<Plot>();
+        }
 
-						
-						java.util.Set<Plot> allPlots = Main.plotAPI.getAllPlots();
+        (new BukkitRunnable() {
+            public void run() {
 
-						for (Plot plot : allPlots) {
+                long start = System.currentTimeMillis();
+                ArrayList<Plot> expiredPlots = new ArrayList<Plot>();
 
-							long[] temp = MCUtils.getExpireDateAndUpdate(plot);
+                ArrayList<Plot> deletedPlots = new ArrayList<Plot>();
+                ArrayList<Plot> failedPlots = new ArrayList<Plot>();
 
-							if (System.currentTimeMillis() + 1000L > temp[0]) {
-								expiredPlots.add(plot);
-							}
 
-						}
-						
-						
-						for(Plot plot : expiredPlots) {
-														
-							
-							if (plot.getRunning() > 0) {
-								failedPlots.add(plot);
-								break;
-							}
-							
-							
-							boolean result = plot.deletePlot(new Runnable() {
-								@Override
-								public void run() {
-									plot.removeRunning();
-									deletedPlots.add(plot);
-								}
-							
-							});
-							if (result) {
-								plot.addRunning();
-								
-							}
-						}
-						
-						
-						long took = System.currentTimeMillis() - start;
-						
-						
-						String deleted = Lang.withPlaceHolder(Lang.DELETE_EXPIRED_INFO, new String[] {"%time%", "%deleted_plots%"}, took, deletedPlots);
-						
-						String failed = Lang.DELETE_EXPIRED_FAILED_RUNNING.toString().replaceAll("%failed_plots%",String.valueOf(failedPlots) );
+                java.util.Set<Plot> allPlots = Main.plotAPI.getAllPlots();
 
-						sender.sendMessage(deleted);
-						System.out.println(deleted);
-						sender.sendMessage(failed);
-						System.out.println(failed);
-					
-					}
+                for (Plot plot : allPlots) {
 
-				}).runTaskAsynchronously(Main.plugin);
+                    long[] temp = MCUtils.getExpireDateAndUpdate(plot);
 
-				return true;
+                    if (System.currentTimeMillis() + 1000L > temp[0]) {
+                        expiredPlots.add(plot);
+                    }
 
-			} else {
+                }
 
-				sender.sendMessage(Lang.DELETE_EXPIRED_HELP.toString());
-				return true;
-			}
 
-		} else {
+                for (Plot plot : expiredPlots) {
 
-			sender.sendMessage(Lang.NO_PERM.toString());
-			return true;
 
-		}
+                    if (plot.getRunning() > 0) {
+                        failedPlots.add(plot);
+                        break;
+                    }
 
-	}
+
+                    boolean result = plot.deletePlot(new Runnable() {
+                        @Override
+                        public void run() {
+                            plot.removeRunning();
+                            deletedPlots.add(plot);
+                        }
+
+                    });
+                    if (result) {
+                        plot.addRunning();
+
+                    }
+                }
+
+
+                long took = System.currentTimeMillis() - start;
+
+
+                String deleted = Lang.withPlaceHolder(Lang.DELETE_EXPIRED_INFO, new String[]{"%time%", "%deleted_plots%"}, took, deletedPlots);
+
+                String failed = Lang.DELETE_EXPIRED_FAILED_RUNNING.toString().replaceAll("%failed_plots%", String.valueOf(failedPlots));
+
+                sender.sendMessage(deleted);
+                System.out.println(deleted);
+                sender.sendMessage(failed);
+                System.out.println(failed);
+
+            }
+
+        }).runTaskAsynchronously(Main.plugin);
+
+        return true;
+
+
+    }
 
 }
